ea2084e556cf14e0480f3a5e30ed08f844bd34dd
Chadly, Ryan <chadlyryan@gmail.com>
2023-06-24 23:35:58 +0200

    working api call

--- components/home-screen.tsx
+++ components/home-screen.tsx
@@ -1,14 +1,52 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, TextInput, Button, Text } from "react-native";
 
 export function HomeScreen() {
+  const [cityName, setCityName] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const apiKey = "a37c89ea6fbc1f0b0f4a2108870bb976";
+
+  const fetchWeatherData = async () => {
+    try {
+      const response = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=a37c89ea6fbc1f0b0f4a2108870bb976`
+      );
+      const data = await response.json();
+      console.log(data);
+      setWeatherData(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setCityName}
+        value={cityName}
+        placeholder="Choose a city..."
+      />
+      <Button title="Get Weather" onPress={fetchWeatherData} />
+      {weatherData && (
+        <View>
+          <Text>City: {weatherData.name}</Text>
+          <Text>Temperature: {weatherData.main.temp}</Text>
+          <Text>Description: {weatherData.weather[0].description}</Text>
+        </View>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: "center", justifyContent: "center" },
+  container: { flex: 1 },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    margin: 10,
+    padding: 5,
+  },
 });
+
+// https://api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}
