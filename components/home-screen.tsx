7112ab24f68c1e9f0312546bb94b21c2995738d6
Chadly, Ryan <chadlyryan@gmail.com>
2023-06-25 07:30:37 +0200

    error handling

--- components/home-screen.tsx
+++ components/home-screen.tsx
@@ -1,23 +1,31 @@
 import React, { useState } from "react";
 import { View, StyleSheet, TextInput, Button, Text, Alert } from "react-native";
 
-export function HomeScreen() {
+export function HomeScreen({ navigation }) {
   const [cityName, setCityName] = useState("");
   const [weatherData, setWeatherData] = useState(null);
   const apiKey = "a37c89ea6fbc1f0b0f4a2108870bb976";
 
   const fetchWeatherData = async () => {
     try {
+      if (!cityName) throw new Error("City name is required.");
       const response = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=a37c89ea6fbc1f0b0f4a2108870bb976`
-      );
+        `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${apiKey}&units=metric`
+      ).then((response) => {
+        if (response.ok) {
+          return response;
+        } else {
+          throw new Error("This city is not found.");
+        }
+      });
       const data = await response.json();
       setWeatherData(data);
+      setCityName("");
+      navigation.navigate("Details", { weatherData: data });
     } catch (error) {
       Alert.alert("Error", error.message, [
         { text: "OK", onPress: () => setCityName("") },
       ]);
-      console.error(error);
     }
   };
   return (
