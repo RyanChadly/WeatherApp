b06905137f5ffc5a8a4b6ab02b98be617b79411f
Chadly, Ryan <chadlyryan@gmail.com>
2023-06-25 07:44:06 +0200

    extract navigation

--- App.tsx
+++ App.tsx
@@ -1,16 +1,14 @@
-import { StyleSheet } from "react-native";
 import { CustomSplashScreen } from "./components/splash-screen";
 import { useEffect, useState } from "react";
-import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { DetailsScreen } from "./components/details-screen";
-import { HomeScreen } from "./components/home-screen";
+import { Navigation } from "./components/navigation";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [showContent, setShowContent] = useState(false);
 
+  // there is nothing to fetch that would require a loading screen, so I am artificially delaying it.
   useEffect(() => {
     const timeout = setTimeout(() => {
       setShowContent(true);
@@ -21,19 +19,5 @@ export default function App() {
 
   if (!showContent) return <CustomSplashScreen />;
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <Navigation />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-  },
-});
